4367f20aa294be29be765aa444132ca7ea608d7e
vjuneja <vanuj85@yahoo.co.in>
2020-05-04 03:02:10 -0700

    current-> old

--- old/service-worker.js
+++ old/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4a3647b74eb69f513cb410cb5f36e416.js"
+  "./precache-manifest.19160aa7a6be61fa9e633539297e048c.js"
 );
 
 self.addEventListener('message', (event) => {
