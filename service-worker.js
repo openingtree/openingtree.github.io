16a71230617b4faa70926631387cee44cb20814b
vjuneja <vanuj85@yahoo.co.in>
2020-07-06 00:07:42 -0700

    Release 4.0.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1cdb1b57f2a99bef395b39593ecc7e74.js"
+  "./precache-manifest.e478813e3cbb659093388ce2901938f2.js"
 );
 
 self.addEventListener('message', (event) => {
